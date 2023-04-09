c3c9c29f79ac0b255d72b5effb6b2fe0454ceb2e
Jędrzej Małaczyński <jedrzejmalaczynski@gmail.com>
2023-04-09 17:00:21 +0200

    czwarte

--- projektweb/public/script.js
+++ projektweb/public/script.js
@@ -12,12 +12,20 @@ function App() {
       alert('Wszystkie pola są wymagane.');
       return;
     }
+    // Tutaj możesz dodać dalsze kroki, np. przesłanie formularza do serwera
   };
 
   return (
     <BrowserRouter>
       <div className="App">
-        {/* ... (header, about, application, footer) */}
+        <header>
+          <h1>Szkoła Letnia</h1>
+        </header>
+
+        <section id="about">
+          <h2>O szkole letniej</h2>
+          <p>Nasza szkoła letnia oferuje szereg interesujących kursów z różnych dziedzin. Zapraszamy do zapoznania się z ofertą i zapisów!</p>
+        </section>
 
         <section id="courses">
           <h2>Dostępne kursy</h2>
@@ -36,6 +44,32 @@ function App() {
             </li>
           </ul>
         </section>
+
+        <section id="application">
+          <h2>Zapisy na kursy</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Imię:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <button type="submit">Zapisz się</button>
+          </form>
+        </section>
+
+        <footer>
+          {/* Miejsce na informacje o prawach autorskich i dane kontaktowe */}
+          <p>&copy; 2023 Szkoła Letnia. Wszystkie prawa zastrzeżone.</p>
+        </footer>
 
         {/* Dodajemy routing dla poszczególnych stron kursów */}
         <Route path="/kurs-programowania-python" component={CoursePython} />
